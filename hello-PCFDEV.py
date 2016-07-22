8e60ede93b9fa1c9392473bae3d6fe5518137bcf
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-07-22 11:14:52 +0200

    Update hello-PCFDEV.py

--- hello-PCFDEV.py
+++ hello-PCFDEV.py
@@ -48,4 +48,4 @@ def hello():
 	""".format(COLOR,my_uuid, counterK,connectionsvarK,whoamiK,ipK)
 
 if __name__ == "__main__":
-app.run(debug=False,host='0.0.0.0', port=env.port)
+	app.run(debug=False,host='0.0.0.0', port=env.port)
